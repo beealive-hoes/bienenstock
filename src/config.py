5c745e71d75c0645b3bf13aa1009f250ec24e177
maanex <maan91315@gmail.com>
2020-02-20 14:15:21 +0100

    stream endpoint

--- src/config.py
+++ src/config.py
@@ -6,9 +6,15 @@ server = {
   'api_url': 'http://localhost:9010/api/'
 }
 
+# server = {
+#   'url': 'http://gh.selfip.org:24252/',
+#   'api_url': 'http://gh.selfip.org:24252/api/'
+# }
+
 api = {
   'endpoints': {
-    'ping': server['api_url'] + 'ping'
+    'ping': server['api_url'] + 'ping',
+    'stream': server['api_url'] + 'stream'
   }
 }
 
