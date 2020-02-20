e458181828be75a3cbc99424e9256d11c3dcd86c
Daaren <killxcrafter@gmail.com>
2020-02-20 13:52:03 +0100

    record sequence

--- src/record.py
+++ src/record.py
@@ -5,14 +5,18 @@ cam = PiCamera()
 cam.resolution = (1920, 1080)
 cam.framerate = 30
 
-def record(recordingTime):
-    cam.start_recording('/media/pi/9401-7CA11/recording.h264') #9401-7CA11 ersetzen durch den Ordner der SSD
-    cam.wait_recording(recordingTime)
-    cam.stop_recording()
+def record(recordingTime, pieces):
+    directory = "/home/pi/bienenstock/"
+    for filename in cam.record_sequence(
+        directory + 'clip%d.h264' % i for i in range(pieces)):
+        cam.wait_recording(recordingTime)
+    
+    for i in range(pieces):
+        from subprocess import CalledProcessError
+        command = "MP4Box -add " + directory + "clip%d.h264 " % i + directory + "clip%d.mp4" % i
+        try:
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+        except subprocess.CalledProcessError as e:
+            print('FAIL:\n cmd:{}\n output:{}'.format(e.cmd, e.output))
 
-    from subprocess import CalledProcessError
-    command = "MP4Box -add /media/pi/9401-7CA11/recording.h264 /media/pi/9401-7CA11/recording.mp4"
-    try:
-        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-    except subprocess.CalledProcessError as e:
-        print('FAIL:\n cmd:{}\n output:{}'.format(e.cmd, e.output))
+record(5, 3)
